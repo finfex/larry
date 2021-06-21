914ca5d1e6068d01b37cbd129098f0264573d264
Danil Pismenny <danil@brandymint.ru>
2021-06-21 09:40:30 +0300

    hotfix

--- app/models/openbill_service.rb
+++ app/models/openbill_service.rb
@@ -3,16 +3,9 @@
 # frozen_string_literal: true
 
 class OpenbillService < ApplicationRecord
-  SERVICES = Billing::SERVICES.keys
-
-  belongs_to :account, class_name: OpenbillAccount
+  belongs_to :account, class_name: 'OpenbillAccount'
   has_many :vendor_services, dependent: :delete_all
   has_many :openbill_transactions
 
   validates :title, uniqueness: true
-
-  def find_by_key!(key)
-    account_id = Billing::SERVICES[key] || raise("Не найден сервис с ключем #{key}")
-    find_by(account_id: account_id) || raise("Не найден сервис с ключем #{key} и счетом #{account_id}")
-  end
 end
