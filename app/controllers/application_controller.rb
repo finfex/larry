dab5de6c365ad2a4b0d2765a45dd460ab9e87fcd
Danil Pismenny <danil@brandymint.ru>
2021-06-21 11:50:21 +0300

    Add Gera::DirectionRatesRepository::NoActualSnapshot rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,15 @@ class ApplicationController < ActionController::Base
   include RailsWarden::Authentication
   layout 'simple'
 
+  rescue_from Gera::DirectionRatesRepository::NoActualSnapshot, with: :rescue_humanized_error
+
   helper_method :current_admin_user
 
   private
+
+  def rescue_humanized_error(exception)
+    render :exception, locals: { exception: exception }
+  end
 
   def current_admin_user
     user :admin_user
