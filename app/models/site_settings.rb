f7dbfa6131fe179c49adccbd152b24fc32db9f6f
Danil Pismenny <danil@brandymint.ru>
2021-06-28 12:27:58 +0300

    Fix site settings

--- app/models/site_settings.rb
+++ app/models/site_settings.rb
@@ -4,18 +4,19 @@
 
 module SiteSettings
   UnknownSettingsKey = Class.new StandardError
+  KEYS = %w[extra_html]
   class SiteSetting < ApplicationRecord
-    validates :key, presence: true, uniqueness: true
+    validates :key, presence: true, uniqueness: true, inclusion: { in: KEYS }
   end
 
   def self.method_missing(key)
     ss = SiteSetting.find_by(key: key)
     return ss.value if ss.present?
 
-    raise UnknownSettingsKey
+    raise SiteSettings::UnknownSettingsKey unless KEYS.include? key.to_s
   end
 
-  def self.respond_to_missing?(_key)
-    true
+  def self.respond_to_missing?(key, _options)
+    super || KEYS.include?(key)
   end
 end
