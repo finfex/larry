d900452d1b647abb55baea8a43491f401f4d1fa9
Danil Pismenny <danil@brandymint.ru>
2021-07-02 20:18:53 +0300

    Fix page regexp

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,7 +10,7 @@ class Page < ApplicationRecord
   validates :menu_title, presence: true, uniqueness: true
   validates :html_title, presence: true
   validates :path, presence: true, uniqueness: true,
-                   format: { with: /\A[a-z0-9-_]+\z/, message: 'Путь может содержать только латиницу, цифры, минус и подчеркивание' }
+                   format: { with: /\A[a-z0-9_][a-z0-9\-_]+\z/, message: 'Путь может содержать только латиницу, цифры, минус и подчеркивание' }
   validates :content, presence: true
 
   has_rich_text :content
