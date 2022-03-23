19ea8d13486e0d917c3931087734296bc7919e5d
Danil Pismenny <danil@brandymint.ru>
2022-03-23 14:01:04 +0300

    m

--- db/migrate/20220323085249_add_not_null_of_maximal_and_minimal_amounts.gera.rb
+++ db/migrate/20220323085249_add_not_null_of_maximal_and_minimal_amounts.gera.rb
@@ -5,6 +5,8 @@ class AddNotNullOfMaximalAndMinimalAmounts < ActiveRecord::Migration[5.2]
     change_column_default :gera_payment_systems, :maximal_outcome_amount_cents, 1000
     change_column_default :gera_payment_systems, :minimal_outcome_amount_cents, 1
 
+    Gera::PaymentSystem.update_all maximal_income_amount_cents: 1000, maximal_outcome_amount_cents: 1000, minimal_outcome_amount_cents: 1
+
     change_column_null :gera_payment_systems, :maximal_income_amount_cents, false
     change_column_null :gera_payment_systems, :maximal_outcome_amount_cents, false
     change_column_null :gera_payment_systems, :minimal_outcome_amount_cents, false
