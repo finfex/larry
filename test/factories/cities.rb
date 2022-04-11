228e88a3aca6610113cf9086220b819de37b25e4
Danil Pismenny <danil@brandymint.ru>
2022-04-11 15:23:16 +0300

    Exclude db/migrate from rubocop

--- test/factories/cities.rb
+++ test/factories/cities.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2019 Danil Pismenny <danil@brandymint.ru>
+
 FactoryBot.define do
   factory :city do
-    name { "MyString" }
+    name { 'MyString' }
   end
 end
