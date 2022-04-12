fed1a79839302e6bf87b74d85b9b0b445faf6f24
Danil Pismenny <danil@brandymint.ru>
2022-04-12 12:28:53 +0300

    Improve db:seeds

--- config/environments/_add_gem_paths.js
+++ config/environments/_add_gem_paths.js
@@ -4,7 +4,7 @@
  */
 function add_paths_to_environment(environment) {
   environment.resolvedModules.add(
-    { key: 'gem-best_in_place', value: '/home/danil/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/bundler/gems/best_in_place-88eb3052623a/lib/assets/javascripts' }
+    { key: 'gem-best_in_place', value: '/home/danil/.rbenv/versions/3.0.3/lib/ruby/gems/3.0.0/bundler/gems/best_in_place-88eb3052623a/lib/assets/javascripts' }
   )
 }
 exports.add_paths_to_environment = add_paths_to_environment
