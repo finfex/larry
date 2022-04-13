a7474ad48a9427cea07058d2e457a29e0c3bbb49
Danil Pismenny <danil@brandymint.ru>
2022-04-13 13:30:41 +0300

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = Settings.smtp_settings.symbolize_keys
+  config.action_mailer.smtp_settings = Rails.configuration.settings.smtp_settings.symbolize_keys
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
