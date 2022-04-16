793b710a7a2dc695d658f5e31896c2b144af876b
Danil Pismenny <danil@brandymint.ru>
2022-04-16 16:38:29 +0300

    Make notificator async

--- app/services/notificator.rb
+++ app/services/notificator.rb
@@ -1,11 +1,15 @@
 module Notificator
-  class << self
-    def new_order(order)
-      ClientMailer.new_order(order).deliver_later
-      SupportMailer.new_order(order).deliver_later
-      AdminUser.where.not(telegram_id: nil).find_each do |au|
-        Telegram.bot.send_message(chat_id: au.telegram_id, text: "Новая заявка #{order.operator_order_url} на сумму #{order.income_amount.format}")
-      end
+  include Sidekiq::Worker
+
+  def self.new_order(order)
+    perform_async order
+  end
+
+  def new_order(order)
+    ClientMailer.new_order(order).deliver_later
+    SupportMailer.new_order(order).deliver_later
+    AdminUser.where.not(telegram_id: nil).find_each do |au|
+      Telegram.bot.send_message(chat_id: au.telegram_id, text: "Новая заявка #{order.operator_url} на сумму #{order.income_amount.format}")
     end
   end
 end
