a4c6d64726438c94c6de9e1675071557e4dde16a
Danil Pismenny <danil@brandymint.ru>
2022-04-16 20:43:50 +0300

    Handle ActionView::MissingTemplate

--- app/controllers/concerns/handle_errors.rb
+++ app/controllers/concerns/handle_errors.rb
@@ -5,9 +5,20 @@ module HandleErrors
     rescue_from WalletSelector::NoWallet, with: :handle_no_wallet
     rescue_from Gera::DirectionRatesRepository::NoActualSnapshot, with: :rescue_humanized_error
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    # Source https://github.com/rails/rails/issues/4127
+    rescue_from ActionView::MissingTemplate, :with => :catch_unacceptable_requests
   end
 
   private
+
+  def catch_unacceptable_requests(exception)
+    if !ActionController::MimeResponds::Collector.new(collect_mimes_from_class_level).negotiate_format(request)
+      head :not_acceptable
+    else
+      raise exception
+    end
+  end
 
   def not_found
     render :not_found, layout: 'simple'
