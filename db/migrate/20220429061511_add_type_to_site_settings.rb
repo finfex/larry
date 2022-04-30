6d70ea9b14b7797bfa505f8523fe139cf5fef471
Danil Pismenny <danil@brandymint.ru>
2022-04-30 09:37:18 +0300

    Add bot_token settings

--- db/migrate/20220429061511_add_type_to_site_settings.rb
+++ db/migrate/20220429061511_add_type_to_site_settings.rb
@@ -2,6 +2,6 @@ class AddTypeToSiteSettings < ActiveRecord::Migration[6.1]
   def up
     add_column :site_settings, :value_type, :string, null: false, default: 'string'
 
-    SiteSettings::SiteSetting.update_all value_type: 'text'
+    SiteSettings.update_all value_type: 'text'
   end
 end
