579d239cf92a4fb1d16b2faf4fe6e281b809974f
Danil Pismenny <danil@brandymint.ru>
2022-07-15 15:31:17 +0300

    Update worker

--- app/workers/vacuum_worker.rb
+++ app/workers/vacuum_worker.rb
@@ -1,6 +1,8 @@
 class VacuumWorker
   include Sidekiq::Worker
 
+  sidekiq_options unique_across_workers: true, queue: 'default', lock: :until_and_while_executing
+
   def perform
     ActiveRecord::Base.descendants.each {|ar| ar.vacuum }
   end
